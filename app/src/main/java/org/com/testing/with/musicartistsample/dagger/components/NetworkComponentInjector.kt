448b437b87ab0ab60d551ba166652370d8c6ec12
joNA <jitzian@gmail.com>
2021-01-26 12:26:02 -0500

    ma jc - Setting up dependency injection of NetworkModule(Retrofit) for the BaseViewModule

--- app/src/main/java/org/com/testing/with/musicartistsample/dagger/components/NetworkComponentInjector.kt
+++ app/src/main/java/org/com/testing/with/musicartistsample/dagger/components/NetworkComponentInjector.kt
@@ -1,4 +1,22 @@
 package org.com.testing.with.musicartistsample.dagger.components
 
-class NetworkComponentInjector {
+import dagger.Component
+import org.com.testing.with.musicartistsample.base.BaseViewModel
+import org.com.testing.with.musicartistsample.dagger.modules.NetworkModule
+
+@Component(
+    modules = [
+        NetworkModule::class
+    ]
+)
+interface NetworkComponentInjector {
+
+    fun inject(baseViewModel: BaseViewModel)
+
+    @Component.Builder
+    interface Builder {
+        fun networkModule(networkModule: NetworkModule): Builder
+        fun build(): NetworkComponentInjector
+    }
+
 }
