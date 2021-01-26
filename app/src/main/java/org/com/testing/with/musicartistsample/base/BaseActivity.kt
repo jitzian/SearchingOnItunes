b9f8ba33426c7ec5f92cd3f09a12e485dacf57d4
joNA <jitzian@gmail.com>
2021-01-26 11:47:38 -0500

    ma jc - Creating BaseActivity and setting up the connectivity checking

---
+++ app/src/main/java/org/com/testing/with/musicartistsample/base/BaseActivity.kt
@@ -0,0 +1,35 @@
+package org.com.testing.with.musicartistsample.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import org.com.testing.with.networkutil.NetworkMonitorUtil
+import kotlin.properties.Delegates
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    protected lateinit var TAG: String
+    protected var networkMonitor: NetworkMonitorUtil by Delegates.notNull()
+    protected var isConnected = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState).also {
+            networkMonitor = NetworkMonitorUtil(this)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart().also {
+            networkMonitor.register()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop().also {
+            networkMonitor.unregister()
+        }
+    }
+
+    abstract fun setupObservers()
+
+}
