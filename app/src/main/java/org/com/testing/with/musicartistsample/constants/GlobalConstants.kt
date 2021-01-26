b93f0c4cc893a8f3a1db92d30f4749d2d7c91195
joNA <jitzian@gmail.com>
2021-01-26 17:29:45 -0500

    ma jc - Mapping variables for setting name and version in db

--- app/src/main/java/org/com/testing/with/musicartistsample/constants/GlobalConstants.kt
+++ app/src/main/java/org/com/testing/with/musicartistsample/constants/GlobalConstants.kt
@@ -5,6 +5,10 @@ class GlobalConstants {
     companion object {
         //https://itunes.apple.com/search?term={any ArtistName}
         const val baseUrl = "https://itunes.apple.com/"
+
+        //DataBase variables
+        const val nameDb = "artistDd"
+        const val versionDb = 1
     }
 
 }
