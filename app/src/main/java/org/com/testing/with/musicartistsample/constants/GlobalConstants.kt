de3ae461159615bd325e845c954f52694f323d00
joNA <jitzian@gmail.com>
2021-01-26 10:47:37 -0500

    ma jc - Creating object which will host the global constants used accross the app

---
+++ app/src/main/java/org/com/testing/with/musicartistsample/constants/GlobalConstants.kt
@@ -0,0 +1,10 @@
+package org.com.testing.with.musicartistsample.constants
+
+class GlobalConstants {
+
+    companion object {
+        //https://itunes.apple.com/search?term={any ArtistName}
+        const val baseUrl = "https://itunes.apple.com"
+    }
+
+}
