3388cbb69306b71704d7955ef152aa8ed4c8a0dc
joNA <jitzian@gmail.com>
2021-01-27 10:38:48 -0500

    ma jc - Implementing cache mechanism. First I am checking if there is data that matches with the input that has been provided. If there is data, then I am displaying what is saved on cache, otherwise, I am fetching from remote API and storing the result into the db and finally presenting it, into the UI. With this I am saving unnecessary data usage
    Cleanup is required

--- app/src/main/java/org/com/testing/with/showArtist/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/org/com/testing/with/showArtist/ui/viewmodels/MainViewModel.kt
@@ -1,21 +1,122 @@
-package org.com.testing.with.ui.viewmodels
+package org.com.testing.with.showArtist.ui.viewmodels
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.com.testing.with.musicartistsample.CustomApp
 import org.com.testing.with.musicartistsample.base.BaseViewModel
+import org.com.testing.with.showArtist.model.db.model.ArtistAlbum
+import org.com.testing.with.showArtist.model.repository.ArtistAlbumRepository
 
 class MainViewModel : BaseViewModel() {
+
+    private val repository by lazy {
+        ArtistAlbumRepository(CustomApp.instance.db.artistAlbumDao())
+    }
 
     init {
         TAG = MainViewModel::class.java.simpleName
     }
 
-    //TODO: Implement local cache functionality
-    fun checkLocalData() {
+    private val _artistResult = MutableLiveData<List<ArtistAlbum>>()
 
+    val artistResult: LiveData<List<ArtistAlbum>>
+        get() = _artistResult
+
+    fun _fetchData(artistName: String?) = viewModelScope.launch {
+//        artistName?.let { safeArtistName ->
+//            viewModelScope.launch {
+//                val queryResult = withContext(Dispatchers.IO) {
+//                    repository.getByArtistName(safeArtistName)
+//                }
+//                if (queryResult.asLiveData().value != null) {
+//                    _artistResult.postValue(queryResult.asLiveData().value)
+//                } else {
+//
+//                }
+//            }
+//
+//
+//        }
+
+        if (artistName != null) {
+            val localResult = repository.getByArtistName(artistName).asLiveData()
+            if (localResult.value != null) {
+                _artistResult.postValue(localResult.value)
+            } else {
+                val queryMap = mutableMapOf(
+                    "term" to artistName
+                )
+                val resultOfFetchedData = withContext(Dispatchers.IO) {
+                    restApi.fetchRemoteData(queryMap)
+                }
+
+                resultOfFetchedData.results?.let { safeListOfResults ->
+                    safeListOfResults.forEach {
+                        val tempArtistAlbum = ArtistAlbum(
+                            artistId = it.artistId,
+                            artistName = it.artistName,
+                            trackName = it.trackName,
+                            releaseDate = it.releaseDate,
+                            primaryGenreName = it.primaryGenreName,
+                            trackPrice = it.trackPrice
+                        )
+                        repository.insert(tempArtistAlbum)
+                    }
+                    withContext(Dispatchers.IO) {
+                        _artistResult.postValue(
+                            repository.getByArtistName(artistName).asLiveData().value
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    fun fetchData(artistName: String?) {
+        if (artistName != null) {
+            viewModelScope.launch {
+                withContext(Dispatchers.IO) {
+                    repository.getByArtistName(artistName).collect { listOfArtistAlbums ->
+                        if (listOfArtistAlbums.isNotEmpty()) {
+                            Log.e(TAG, "fetchData::NOT EMPTY::${listOfArtistAlbums.size}")
+                            _artistResult.postValue(listOfArtistAlbums)
+                        } else {
+                            Log.e(TAG, "fetchData::EMPTY::")
+                            val queryMap = mutableMapOf(
+                                "term" to artistName
+                            )
+                            val resultRemoteData = restApi.fetchRemoteData(queryMap)
+                            resultRemoteData.results.apply {
+                                this?.forEach {
+                                    val tempArtistAlbum = ArtistAlbum(
+                                        artistId = it.artistId,
+                                        artistName = it.artistName,
+                                        trackName = it.trackName,
+                                        releaseDate = it.releaseDate,
+                                        primaryGenreName = it.primaryGenreName,
+                                        trackPrice = it.trackPrice
+                                    )
+                                    repository.insert(tempArtistAlbum)
+                                }
+
+                                _artistResult.postValue(
+                                    //TODO: Should I used collect instead of asLiveData().value??
+                                    repository.getByArtistName(artistName).asLiveData().value
+                                )
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
     }
 
     //TODO: Implement
@@ -24,7 +125,6 @@ class MainViewModel : BaseViewModel() {
             val result = withContext(Dispatchers.IO) {
                 val queryMap = mutableMapOf(
                     "term" to "Linkin Park"
-
                 )
                 restApi.fetchRemoteData(queryMap)
             }
